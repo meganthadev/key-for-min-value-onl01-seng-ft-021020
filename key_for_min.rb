b40a90e51c2b0958f4fee9052d599f04c854735a
Megan Horsfield <meganthadev@gmail.com>
2020-02-22 08:10:54 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,14 +1,8 @@
-def key_for_min_value(name_hash)
-  return nil if name_hash == {}
-  temp_key = ""
-  temp_value = ""
-  if (temp_value == "") 
-      temp_key = key
-      temp_value = value
-  elsif (value < temp_value)
-  temp_key = key
-      temp_value = value
-    end
+  def key_for_min_value(name_hash)
+  smallest_hash_key = nil
+  name_hash.each do |key, val|
+    smallest_hash_key ||= key
+    smallest_hash_key = key if val < name_hash[smallest_hash_key]
   end
-  temp_key
-end
+  smallest_hash_key
+end 	end
